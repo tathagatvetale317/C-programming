f2ff834abd08e204e22034c8ca43326e3e8a6818
tathagatvetale317 <tathagatvetale317@eng.rizvi.edu.in>
2025-10-06 16:33:19 +0530

    Experiment_2A.c

---
+++ Eperiment_2A.c
@@ -0,0 +1,15 @@
+/* NAME: TATHAGAT GAUTAM VETALE
+UIN :- 251C025
+CLASS: CIVIL
+DIV :- B */
+#include <stdio.h>
+int main () {
+int x;
+printf("Enter a number:");
+scanf("%d",&x);
+if (x%2==0)
+printf("%d is even",x);
+else
+printf("%d is odd",x);
+return 0;
+}
