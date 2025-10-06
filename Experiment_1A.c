aa6d503b870ba1062a4fb242119806ebb7c4bc9e
tathagatvetale317 <tathagatvetale317@eng.rizvi.edu.in>
2025-10-06 16:12:53 +0530

    Add files via upload

---
+++ Experiment_1A.c
@@ -0,0 +1,11 @@
+/*NAME: Tathagat GAUTAM VETALE
+CLASS: CIVIL BRANCH
+DIVISION: B
+UIN NO: 251C025
+*/
+#include <stdiol.h>
+int main(){
+
+printf("NAME:- TATHAGAT GAUTAM VETALE\n AGE:- 18\n CLASS:- CIVIL\n DIVISION:- B\n UIN: - 251C025 ");
+ return 0;
+}
