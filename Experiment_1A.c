6ca52b6a0048909aab78153006030fe674d79295
tathagatvetale317 <tathagatvetale317@eng.rizvi.edu.in>
2025-10-06 16:14:44 +0530

    Update Experiment_1A.c

--- Experiment_1A.c
+++ Experiment_1A.c
@@ -3,9 +3,10 @@ CLASS: CIVIL BRANCH
 DIVISION: B
 UIN NO: 251C025
 */
-#include <stdiol.h>
-int main(){
-
+#include <stdio.h>
+int main()
+{
 printf("NAME:- TATHAGAT GAUTAM VETALE\n AGE:- 18\n CLASS:- CIVIL\n DIVISION:- B\n UIN: - 251C025 ");
  return 0;
 }
+
