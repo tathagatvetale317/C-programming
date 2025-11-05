568a4cd449011ee27769275056627fa584ee6344
tathagatvetale317 <tathagatvetale317@eng.rizvi.edu.in>
2025-11-05 09:02:15 +0530

    Experiment_8b.c

--- Experiment_8b.c
+++ Experiment_8b.c
@@ -3,28 +3,33 @@ UIN :- 251C025
 CLASS:- CIVIL
 ROLL NO:- 20
 DIV:- B */
-
 #include <stdio.h>
-
 int main() {
     char str[100];
-    int i, length, flag = 0;
+    int i, length = 0, flag = 0;
+    
     printf("Enter a string: ");
-    gets(str);
-
+    fgets(str, sizeof(str), stdin);
+    
     for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
         length++;
     }
-
+    
     for (i = 0; i < length / 2; i++) {
         if (str[i] != str[length - 1 - i]) {
             flag = 1;
             break;
         }
     }
-if (flag == 0)
-    printf("The string is a palindrome.\n");
-else
-    printf("The string is not a palindrome.\n");
-return 0;
+    
+    if (flag == 0)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+    
+    return 0;
 }
